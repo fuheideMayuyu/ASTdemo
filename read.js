a3a7c9481f332c1bb7deba61d44531d2b7de3c87
fuheideMayuyu <xiaguangding@163.com>
2019-03-25 18:11:13 +0800

    修改并重新编译js代码

--- read.js
+++ read.js
@@ -18,14 +18,25 @@ const TNT = recast.types.namedTypes
 // });
 
 // 判断AST对象类型
+// recast.run(function(ast, printSource) {
+//   recast.visit(ast, {
+//       visitExpressionStatement: function(path) {
+//         const node = path.value
+//         // 判断是否为ExpressionStatement，正确则输出一行字。
+//         if(TNT.ExpressionStatement.check(node)){
+//           console.log('这是一个ExpressionStatement')
+//         }
+//         this.traverse(path);
+//       }
+//     });
+// });
+
 recast.run(function(ast, printSource) {
   recast.visit(ast, {
       visitExpressionStatement: function(path) {
-        const node = path.value
-        // 判断是否为ExpressionStatement，正确则输出一行字。
-        if(TNT.ExpressionStatement.check(node)){
-          console.log('这是一个ExpressionStatement')
-        }
+        const node = path.node
+        // 判断是否为ExpressionStatement，正确不输出，错误则全局报错
+        TNT.ExpressionStatement.assert(node)
         this.traverse(path);
       }
     });
